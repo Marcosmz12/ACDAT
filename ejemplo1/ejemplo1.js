09733a505353febf45caaf41ed46768219097bb3
Marcos Molis Zapata <mmolzap0412@g.educaand.es>
2025-09-18 18:14:13 +0200

    Mas ejemplos hechos

---
+++ ejemplo1/ejemplo1.js
@@ -0,0 +1,7 @@
+var nombre;
+var edad;
+nombre = prompt("Dime tu nombre:");
+edad = prompt("Dime tu edad:");
+
+document.write("Tu nombre es: " + nombre);
+document.write(" Tienes " + edad + " años.")
