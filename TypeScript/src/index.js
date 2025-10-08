7851b9a2d8c2366ded62cd7a689e32f4cd755d0c
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-10-08 19:35:32 +0200

    cambios en las carpetas

---
+++ TypeScript/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("World");
+//# sourceMappingURL=index.js.map
