9b7d90c558563791f75b6453a5bd52945c02a5ef
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-10-29 17:51:15 +0100

    Angular

---
+++ Angular/mi-proyecto/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('mi-proyecto');
+  userName: string = 'Marcos';
+  number: number = 0;
+}
