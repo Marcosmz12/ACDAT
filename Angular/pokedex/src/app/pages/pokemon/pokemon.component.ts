aa0e3e04e303024ff1d092e00f4ff0c0a61b759a
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-11-06 17:42:24 +0100

    Pokedex realizada

--- Angular/pokedex/src/app/pages/pokemon/pokemon.component.ts
+++ Angular/pokedex/src/app/pages/pokemon/pokemon.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DigitNumberPipe } from '../../pipes/digit-number.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { PokemonTypeComponent } from '../../components/pokemon-type/pokemon-type.component';
 import { PokemonDetail } from '../../models/pokemon-detail';
 import { PokeapiService } from '../../services/pokeapi.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, async } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
@@ -13,16 +14,27 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
-export class PokemonComponent implements OnInit {
+
+export class PokemonComponent implements OnInit, OnDestroy {
   pokemon: PokemonDetail | null = null;
+  routeParamMap$: Subscription | null = null;
   constructor(
     private pokeapi: PokeapiService,
     private activatedRoute: ActivatedRoute,
     private router: Router) { }
   async ngOnInit(): Promise<void> {
-    const id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as
-      number;
-    this.pokemon = await this.pokeapi.getPokemonDetail(id);
+    // El evento ngOnInit solo se llama una vez en toda la vida del componente.
+    // Por tanto, para poder captar los cambios en la url nos suscribimos al paramMap del activatedRoute.
+    // Cada vez que se cambie la url se llamará al método onInit
+    this.routeParamMap$ = this.activatedRoute.paramMap.subscribe(async paramMap => {
+      const id = paramMap.get('id') as unknown as number;
+      this.pokemon = await this.pokeapi.getPokemonDetail(id);
+    });
+  }
+  ngOnDestroy(): void {
+    // Cuando este componente se destruye hay que cancelar la suscripción.
+    // Si no se cancela se seguirá llamando aunque el usuario no esté ya en esta página
+    this.routeParamMap$?.unsubscribe();
   }
   goPrevious() {
     this.goOtherPokemon(-1);
@@ -34,35 +46,13 @@ export class PokemonComponent implements OnInit {
     let id = this.pokemon?.id as number + offset;
     if (id < 1) {
       id = 151;
-      @Component({
-        selector: 'app-pokemon',
-        standalone: true,
-        imports: [DigitNumberPipe, TitleCasePipe, PokemonTypeComponent],
-        templateUrl: './pokemon.component.html',
-        styleUrl: './pokemon.component.css'
-      })
-      export class PokemonComponent implements OnInit {
-        pokemon: PokemonDetail | null = null;
-        constructor(
-          private pokeapi: PokeapiService,
-          private activatedRoute: ActivatedRoute,
-          private router: Router) { }
-        async ngOnInit(): Promise<void> {
-          const id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as
-            number;
-          this.pokemon = await this.pokeapi.getPokemonDetail(id);
-        }
-        goPrevious() {
-          this.goOtherPokemon(-1);
-        }
-        goNext() {
-          this.goOtherPokemon(+1);
-        }
-        goOtherPokemon(offset: number) {
-          let id = this.pokemon?.id as number + offset;
-          if (id < 1) {
-            id = 151;
-          }
-        }
-      }
+    } else if (id > 151) {
+      id = 1;
     }
+    this.router.navigateByUrl(`pokemon/${id}`);
+  }
+}
+
+
+
+
