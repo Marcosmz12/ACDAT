138eba68afe6c18a723d0c558b43d4018174d50c
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-11-13 18:15:34 +0100

    Ejercicio chistes de chuck norris

--- Angular/chistes-consumo-api/src/app/app.component.ts
+++ Angular/chistes-consumo-api/src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ChistesService } from './chistes.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'chistes-consumo-api';
+  chiste = '';
+  title ='chistes-consumo-api';
+
+  constructor(private chistesService: ChistesService) {}
+
+  cargaChiste() {
+    this.chiste = 'Cargando chiste...';
+    this.chistesService.getChiste().subscribe(
+    nuevoChiste => this.chiste = nuevoChiste
+    )
+  }
 }
